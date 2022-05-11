a7cfc6da549d3d2765c13a7350b49eb1e7dbf265
TimKDev <99671546+TimKDev@users.noreply.github.com>
2022-05-11 12:00:46 +0200

    worked on dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild, OnInit} from '@angular/core';
 import DatalabelsPlugin from 'chartjs-plugin-datalabels';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Order } from 'src/models/order.class';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,10 +12,17 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class DashboardComponent {
 
+  totalPayments = 0;
+  totalNumber = 0;
+  totalOpenPayments = 0;
+
+  lableChart = ['Phone1', 'Phone2', 'Phone3' ];
+  dataChart = [300, 500, 100];
+
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
   // Pie
-  public pieChartOptions: ChartConfiguration['options'] = {
+  pieChartOptions: ChartConfiguration['options'] = {
     responsive: true,
     maintainAspectRatio: false, // Diese Option muss hinzugefügt werden, damit sich das Diagramm an die Größe des 
     // Canvas anpasst und nicht umgekehrt! 
@@ -31,14 +40,47 @@ export class DashboardComponent {
       },
     }
   };
-  public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: ['Phone1', 'Phone2', 'Phone3' ],
+  pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: this.lableChart,
     datasets: [ {
-      data: [ 300, 500, 100 ]
+      data: this.dataChart
     } ]
   };
-  public pieChartType: ChartType = 'pie';
-  public pieChartPlugins = [ DatalabelsPlugin ];
+  pieChartType: ChartType = 'pie';
+  pieChartPlugins = [ DatalabelsPlugin ];
+
+
+  allOrders: any = [];
+
+  constructor(
+    private firestore: AngularFirestore
+    ) { }
+
+  ngOnInit(): void {
+    this.firestore
+    .collection('orders')
+    .valueChanges({idField: 'orderIdName'}) 
+    .subscribe((changes: any) => {
+      this.allOrders = changes.filter((order: Order) => {
+        return !(order.status == 'cancled');
+      });
+      console.log(this.allOrders);
+    });
+  }
+
+  resetStatistics() {
+    this.totalPayments = 0;
+    this.totalNumber = 0;
+    this.totalOpenPayments = 0;
+    
+  }
+
+  calcStatistics() {
+    this.allOrders
+    .forEach((order: Order) => {
+      
+    });
+  }
 
  
 
